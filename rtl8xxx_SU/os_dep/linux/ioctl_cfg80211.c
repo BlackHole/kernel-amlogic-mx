8a9aefefce7c52c7ace3e5bdf2b97720ab0aeea8
root <root@vm-debian64.corp.generali.net>
2016-07-01 13:50:12 +0200

    fix error: ‘CFG80211_SME_CONNECTING’ undeclared

--- rtl8xxx_SU/os_dep/linux/ioctl_cfg80211.c
+++ rtl8xxx_SU/os_dep/linux/ioctl_cfg80211.c
@@ -393,6 +393,7 @@ void rtw_cfg80211_indicate_disconnect(_adapter *padapter)
 		return;
 	}
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,11,0)
 	if(pwdev->sme_state==CFG80211_SME_CONNECTING)
 		cfg80211_connect_result(padapter->pnetdev, NULL, NULL, 0, NULL, 0, 
 							WLAN_STATUS_UNSPECIFIED_FAILURE, GFP_ATOMIC/*GFP_KERNEL*/);
@@ -401,6 +402,10 @@ void rtw_cfg80211_indicate_disconnect(_adapter *padapter)
 			   				NULL, 0, GFP_ATOMIC);
 	else
 		printk("pwdev->sme_state=%d\n", pwdev->sme_state);
+#else
+	cfg80211_disconnected(padapter->pnetdev, 0,
+		   				NULL, 0, GFP_ATOMIC);
+#endif
 
 }
  	
